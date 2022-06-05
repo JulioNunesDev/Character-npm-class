e80d5844157d83019eaf3455b1eac23c9aa6888e
julionunesdev <julionunes999@gmail.com>
2022-06-05 05:21:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Projeto de estudos
+
+
+Example
+
+
+let person = new Math()
+
+person.character(300,120,10)
+
+<pre>
+    <code>
+    //import Character from 'class-character' for commojs or
+    // let Character = require('class-character' for module)
+
+        let person = new Character() 
+        person.character(300,120,10) // methods character(hp, atk, luck)
+
+    </code>
+</pre>
---
+++ index.js
@@ -0,0 +1,27 @@
+class Character {
+
+
+    soma(...valores){
+        let soma = 0;
+        for(var i = valores.length - 1; i>= 0 ; i-- ){
+            soma += Number(valores[i])
+        }
+        return console.log("Total do Poder " + soma)
+    }
+
+    character(life, atk, luck){
+        
+        var currentLife = {
+            profile: {hp: Number(life), attack: Number(atk), luck: Number(luck)}
+        }
+
+       console.log(`HP:${currentLife.profile.hp}, ATK:${currentLife.profile.attack}, LUCK:${currentLife.profile.luck}`)
+
+
+        return this.soma(currentLife.profile.hp, currentLife.profile.attack, currentLife.profile.luck)
+    }
+
+   
+}
+
+
